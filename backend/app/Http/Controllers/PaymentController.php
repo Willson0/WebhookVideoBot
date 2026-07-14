5fcaabce37646fd67a9a171bea507e0941f81bef
nikitalogvinov <logvinovnikita3@gmail.com>
2026-07-15 01:40:36 +0500

    ff

--- backend/app/Http/Controllers/PaymentController.php
+++ backend/app/Http/Controllers/PaymentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Payment;
 use App\Models\User;
 use Carbon\Carbon;
+use DateTime;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
@@ -83,14 +84,20 @@ class PaymentController extends Controller
             if ($request->object["payment_method"]["saved"] === true ?? null) $user->payment_method_id = $request->object["payment_method"]["id"];
 
             $user->paid_money += $payment->rub_summ;
-            if ($payment->package === "trial") $user->tried_trial = True;
+            if ($payment->package === "trial") {
+                $user->tried_trial = True;
+                $user->recurrent_package = "5";
+            }
+            else $user->recurrent_package = $payment->package;
+
+            $next_charge = (new DateTime())->modify('+' . $payment->days . ' days')->getTimestamp();
+            $user->recurrent_time = $next_charge;
 
             $user->save();
             $payment->is_bought = true;
 
             $botToken = env('TELEGRAM_BOT_TOKEN');
-            if ($payment->sub === "tokens") $text = "✅ Оплата успешно прошла! На ваш аккаунт добавлено {$payment->days} токенов";
-            else $text = "<b>🎉 Классно, оплата прошла успешно!</b>\n\n❤️ Спасибо, что выбрали нас!\nЕсли будут вопросы — всегда здесь, чтобы помочь!";
+            $text = "<b>🎉 Классно, оплата прошла успешно!</b>\n\n❤️ Спасибо, что выбрали нас!\nЕсли будут вопросы — всегда здесь, чтобы помочь!";
 
             $data = [
                 'chat_id' => $user->id,
@@ -102,49 +109,7 @@ class PaymentController extends Controller
             Http::post($url, $data);
         } else if (($request->event === "payment.canceled" || $request->status === "canceled") && $payment->is_autopayment) {
             $user = User::find($payment->user_id);
-
-            $try = intval(explode('_', $user->orig_tariff)[1]);
-
-            if (intval($payment->days) === 30) {
-                $PRICES = [
-                    7 => 2,
-//                    7 => 279,
-                    30 => 569,
-                    90 => 1499,
-                    180 => 2899
-                ];
-
-                $this->buy(new Request([
-                    "user_id" => $user->id,
-                    "sub" => "pro",
-                    "days" => 7,
-                    "rub_summ" => $PRICES[7],
-                    "summ" => $PRICES[7],
-                ]));
-            }
-
-            if ($try === 4) {
-                $user->orig_tariff = "free";
-                $user->tariff_time = 0;
-                $user->save();
-                return response("ok", 200);
-            }
-
-            $user->orig_tariff = $payment->sub . "_" . ($try + 1);
-
-            $SPISANIE_TIMES_OSN = [
-                0 => 0,
-                1 => 86400,   # через день
-                2 => 86400,   # через 2 дня
-                3 => 432000,  # через неделю
-                4 => 2116800  # через месяц
-            ];
-            $tryIndex = $try + 1;
-            $seconds = isset($SPISANIE_TIMES_OSN[$tryIndex]) ? (int)$SPISANIE_TIMES_OSN[$tryIndex] : 0;
-            $user->tariff_time = Carbon::now()->addSeconds($seconds)->timestamp;
-
-            $user->start_sub_time = 0;
-
+            if (time() >= $user->recurrent_time) $user->recurrent_package = "0";
             $user->save();
         }
 
