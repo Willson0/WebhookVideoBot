e58976d8472ca0913996a8c055e695f5419a8db1
Willson0 <logvinovnikita3@gmail.com>
2026-07-14 17:50:58 +0500

    first commit

---
+++ backend/app/Http/Controllers/ViewerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PerplexityPrompt;
+use Illuminate\Http\Request;
+
+class ViewerController extends Controller
+{
+    public function get ($hash, Request $request) {
+        $row = PerplexityPrompt::where("hash", $hash)->first();
+        if (!$row || $row->user_id !== $request["initData"]["user"]["id"]) abort (404);
+
+        $row->sources = json_decode($row->sources);
+        return response()->json($row);
+    }
+}
